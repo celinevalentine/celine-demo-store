66c407958e37cc1949963c6e65c4eb0728b0a5f7
Celine Valentine <cyv419@gmail.com>
2022-02-10 14:49:19 -0800

    first commit

---
+++ utils/shopify.js
@@ -0,0 +1,5 @@
+import Client from 'shopify-buy';
+export const client = Client.buildClient({
+  storefrontAccessToken: '3eca5d6930e13acf7a98550729179a5b',
+  domain: 'celine-demo-shop.myshopify.com',
+});
