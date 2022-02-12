965b3c7914ebd25ac7849a74ba99bb1ed1853b04
Celine Valentine <cyv419@gmail.com>
2022-02-11 22:15:12 -0800

    deleted unused directories

--- utils/shopify.js
+++ utils/shopify.js
@@ -1,5 +1,5 @@
 import Client from 'shopify-buy';
 export const client = Client.buildClient({
-  storefrontAccessToken: '3eca5d6930e13acf7a98550729179a5b',
+  storefrontAccessToken: process.env.SHOPIFY_STORE,
   domain: 'celine-demo-shop.myshopify.com',
 });
